4939f2952867554c8a6541a9d05e0a0d0d438c26
ZEYNEP GULHAN USLU <zeynep.uslu@turkcell.com.tr>
2023-09-19 12:07:17 +0300

    add arguments and bug fix for quantize_denoiser_onnx.py

--- Readme.md
+++ Readme.md
@@ -13,7 +13,8 @@ pip install -r requirements.txt
 Aşağıdaki gibi çalıştırabilirsiniz.
 
 ```bash
-python denoiser_stream_onnx_test.py
+python denoiser_stream_onnx_test.py -m ./dns48_depth=4_stride=128.onnx -n audio-wav-16k 
+-o enhanced_dir -b 128 -s 128 -f 480 -v
 ```
 
 zip modeli bir miktar daha ufak, ikisini de deneyebilirsiniz.
--- quantize_denoiser_onnx.py
+++ quantize_denoiser_onnx.py
@@ -1,3 +1,5 @@
+import argparse
+
 import numpy as np
 import onnx
 import onnxruntime
@@ -36,7 +38,8 @@ def convert_quantize_from_onnx(onnx_model_path, quantized_onnx_model_path):
         print("Exported model has been tested with ONNXRuntime, and the result looks good!")
 
 
-def convert_quantize_from_streamtt_onnx(hidden, depth):
+def convert_quantize_from_streamtt_onnx(onnx_model_path, quantized_onnx_model_path,
+                                        hidden, depth, stride, frame_length, resample_buffer):
     quantize_dynamic(Path(onnx_model_path), Path(quantized_onnx_model_path), weight_type=QuantType.QUInt8)
     q_model = onnx.load(quantized_onnx_model_path)
 
@@ -47,14 +50,15 @@ def convert_quantize_from_streamtt_onnx(hidden, depth):
     ort_session = onnxruntime.InferenceSession(onnx_model_path)
     # Assume your model expects a single float32 tensor of shape (1, 3, 224, 224)
     # Here's how you might create a dummy tensor of zeroes of the right type and shape
-
+    variance_tensor = torch.tensor([0.0], dtype=torch.float32)
     input_name = ort_session.get_inputs()[0].name
     frame_num_name = ort_session.get_inputs()[1].name
-    resample_input_frame_name = ort_session.get_inputs()[2].name
-    resample_out_frame_name = ort_session.get_inputs()[3].name
-    conv_state_name = ort_session.get_inputs()[4].name
-    lstm_state_1_name = ort_session.get_inputs()[5].name
-    lstm_state_2_name = ort_session.get_inputs()[6].name
+    variance_input_name = ort_session.get_inputs()[2].name
+    resample_input_frame_name = ort_session.get_inputs()[3].name
+    resample_out_frame_name = ort_session.get_inputs()[4].name
+    conv_state_name = ort_session.get_inputs()[5].name
+    lstm_state_1_name = ort_session.get_inputs()[6].name
+    lstm_state_2_name = ort_session.get_inputs()[7].name
 
     lstm_state_1 = torch.randn(2, 1, hidden * 2 ** (depth - 1))
     lstm_state_2 = torch.randn(2, 1, hidden * 2 ** (depth - 1))
@@ -87,21 +91,15 @@ def convert_quantize_from_streamtt_onnx(hidden, depth):
 
     output_name = ort_session.get_outputs()[0].name
     out_frame = ort_session.get_outputs()[1].name
-    out_resample_in_frame = ort_session.get_outputs()[2].name
-    out_resample_frame = ort_session.get_outputs()[3].name
-    out_conv = ort_session.get_outputs()[4].name
-    out_lstm_1 = ort_session.get_outputs()[5].name
-    out_lstm_2 = ort_session.get_outputs()[6].name
+    out_variance_name = ort_session.get_outputs()[2].name
+    out_resample_in_frame = ort_session.get_outputs()[3].name
+    out_resample_frame = ort_session.get_outputs()[4].name
+    out_conv = ort_session.get_outputs()[5].name
+    out_lstm_1 = ort_session.get_outputs()[6].name
+    out_lstm_2 = ort_session.get_outputs()[7].name
 
     frame_num = torch.tensor([1])
-    frame_length = 661
-    if depth == 4:
 
-        stride = 64
-        resample_buffer = 64
-    else:
-        stride = 256
-        resample_buffer = 256
     noisy = torch.randn(1, frame_length * 2)
 
     resample_input_frame = torch.zeros(1, resample_buffer)
@@ -116,6 +114,7 @@ def convert_quantize_from_streamtt_onnx(hidden, depth):
             input_values = {
                 input_name: frame_np,
                 frame_num_name: to_numpy(frame_num),
+                variance_input_name: to_numpy(variance_tensor),
                 resample_input_frame_name: to_numpy(resample_input_frame),
                 resample_out_frame_name: to_numpy(resample_out_frame),
                 conv_state_name: to_numpy(conv_state),
@@ -128,21 +127,22 @@ def convert_quantize_from_streamtt_onnx(hidden, depth):
 
             print(f"frame number: {frame_num}, frame tensor shape: {frame.shape}, dtype: {frame.dtype}")
 
-            out = ort_session.run([output_name, out_frame, out_resample_in_frame,
+            out = ort_session.run([output_name, out_frame, out_variance_name, out_resample_in_frame,
                                    out_resample_frame, out_conv, out_lstm_1, out_lstm_2],
                                   input_values)
 
             # onnx out#
             output_np = out[0]
-            resample_input_frame = out[2]
-            resample_out_frame = out[3]
-            out_conv_tensor = torch.from_numpy(out[4])
+            variance_input = out[2]
+            resample_input_frame = out[3]
+            resample_out_frame = out[4]
+            out_conv_tensor = torch.from_numpy(out[5])
 
             print(f"out conv tensor shape {out_conv_tensor.shape}")
             print("---------------------------------------------------")
             conv_state = out_conv_tensor
-            lstm_state_1 = out[5]
-            lstm_state_2 = out[6]
+            lstm_state_1 = out[6]
+            lstm_state_2 = out[7]
             outs.append(torch.from_numpy(output_np))
             pending = pending[:, stride:]
             frame_num.add_(1)
@@ -154,6 +154,7 @@ def convert_quantize_from_streamtt_onnx(hidden, depth):
             input_values = {
                 input_name: to_numpy(last_frame),
                 frame_num_name: to_numpy(frame_num),
+                variance_input_name: to_numpy(variance_input),
                 resample_input_frame_name: to_numpy(resample_input_frame),
                 resample_out_frame_name: to_numpy(resample_out_frame),
                 conv_state_name: to_numpy(conv_state),
@@ -161,7 +162,7 @@ def convert_quantize_from_streamtt_onnx(hidden, depth):
                 lstm_state_2_name: to_numpy(lstm_state_2)
             }
 
-            out = ort_session.run([output_name, out_frame, out_resample_in_frame, out_resample_frame,
+            out = ort_session.run([output_name, out_frame, out_variance_name, out_resample_in_frame, out_resample_frame,
                                    out_conv, out_lstm_1, out_lstm_2], input_values)
             output_np = out[0]
             outs.append(torch.from_numpy(output_np))
@@ -173,10 +174,33 @@ def convert_quantize_from_streamtt_onnx(hidden, depth):
 
 
 if __name__ == '__main__':
-    root = 'D:/zeynep/data/noise-cancelling/denoiser/dns/hidden=36/'
-    onnx_model_path = root + "dns36_depth=5_stream.onnx"
-    quantized_onnx_model_path = root + "dns36_depth=5_stream_quantized.onnx"
-    # quantize onnx model
-    #convert_quantize_from_onnx(onnx_model_path, quantized_onnx_model_path)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model', type=str, required=True, help='Onnx model file')
+    parser.add_argument('-q', '--quantized_model', type=str, required=True, help='Quantized model file')
+    parser.add_argument('-s', '--stream', action='store_true',
+                        help='True if onnx model streaming mode.')
+    parser.add_argument('-hs', '--hidden_size', type=int, required=False, default=48,
+                        help='Hidden size for dns model.')
+    parser.add_argument('-d', '--depth', type=int, required=False, default=4,
+                        help='Model depth')
+    parser.add_argument('-f', '--frame_length', type=int, required=False, default=480, help='frame length value')
+    parser.add_argument('-b', '--resample_buffer', type=int, required=False, default=64, help='resample buffer value')
+    parser.add_argument('-stride', '--stride', type=int, required=False, default=64, help='Stride value')
+
+    args = parser.parse_args()
+    onnx_model_file = args.model
+    quantized_model_file = args.quantized_model
+    is_stream = args.stream
+    hidden_size = args.hidden_size
+    depth = args.depth
+    frame_length = args.frame_length
+    resample_buffer = args.resample_buffer
+    stride = args.stride
+
+    if is_stream:
+        convert_quantize_from_streamtt_onnx(onnx_model_file, quantized_model_file, hidden_size,
+                                            depth, stride, frame_length, resample_buffer)
+    else:
+        convert_quantize_from_onnx(onnx_model_file, quantized_model_file)
     # quantize onnx stream model
-    convert_quantize_from_streamtt_onnx(36, 5)
+
